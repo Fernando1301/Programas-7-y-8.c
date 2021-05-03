d6f4115e5c1eb40ab60901e7bf89bec2b365c76d
Fernando1301 <82849769+Fernando1301@users.noreply.github.com>
2021-05-03 18:09:40 -0500

    Add files via upload

---
+++ Programa 7.Sumatoria de Numeros.c
@@ -0,0 +1,15 @@
+//Raiz cuadrada//
+ #include stdio.h
+ int main(){
+    /* Flores Salinas Fernando Andres
+    Equipo "Alfa Buena Maravilla Escuadron Lobo"
+    23 abr 21
+    Programa 8. Raiz cuadrado
+    */
+    int R, Y;
+    printf("Introduzca un numero");
+    scanf("%d , &Y);
+    R= Raiz de (Y)
+    printf("El valor de la raiz cuadrada es %d");
+    return 0;
+ }
---
+++ Programa 8. Raiz Cuadrada.c
@@ -0,0 +1,17 @@
+ //Suma de numeros//
+ #include stdio.h
+ int main(){
+    /* Flores Salinas Fernando Andres
+    Equipo "Alfa Buena Maravilla Escuadron Lobo"
+    23 abr 21
+    Programa 7. Sumatoria de numeros
+    */
+    int A, B, C;
+    printf("Introduzca un numero");
+    scanf("%d , &B);
+    printf("Brindame otro numero");
+    scanf("%d , &C);
+    A= B + C;
+    printf("El valor de A es %d");
+    return 0;
+ }
